6bf29db0d78156110f8cf74def58864afbef720e
Rabiul Islam Niloy <rabulislamniloy@gmail.com>
2025-11-19 22:46:22 +0600

    Beecrowd Problem

---
+++ 2752.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char s[50] = "AMO FAZER EXERCICIO NO URI";
+    printf("<%s>\n",s);
+    printf("<%30s>\n",s);
+    printf("<%.20s>\n",s);
+    printf("<%-20s>\n",s);
+    printf("<%-30s>\n",s);
+    printf("<%.30s>\n",s);
+    printf("<%30.20s>\n",s);
+    printf("<%-30.20s>\n",s);
+
+    return 0;
+}
+
